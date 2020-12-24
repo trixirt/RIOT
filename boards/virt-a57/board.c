f6b26a2686f3c3345be2ed98e04b18b1a2749f49
Tom Rix <trix@redhat.com>
2020-12-23 19:48:21 -0500

    stub in qemu aarch64 virt board for a57

---
+++ boards/virt-a57/board.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2020 Tom Rix
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include "board.h"
+
+void board_init(void)
+{
+    /* initialize the CPU */
+    cpu_init();
+
+}
---
+++ boards/virt-a57/include/board.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2020 Tom Rix
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "cpu.h"
+
+#endif /* BOARD_H */
+
